0fc366635478b0c1774a43d8b475d8f99f84861a
Saf-X <safwaanshareef1@gmail.com>
2025-09-24 01:33:13 +0100

    Button mask logic

--- main.py
+++ main.py
@@ -18,34 +18,35 @@ ASSETS_DIR = os.path.join(MAIN_DIR, 'assets')
 IMAGES_DIR = os.path.join(ASSETS_DIR, 'images')
 SOUNDS_DIR = os.path.join(ASSETS_DIR, 'sounds')
 
-# Initialise display
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pg.display.set_caption("Maze Master")
-pg.display.set_icon(pg.image.load(os.path.join(IMAGES_DIR, 'maze_icon.png')))
-
 # Global functions
 
 
 # Classes
 class Game:
     def __init__(self):
+        # Initialise screen display
         self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pg.display.set_caption("Maze Master")
         pg.display.set_icon(pg.image.load(os.path.join(IMAGES_DIR, 'maze_icon.png')))
+
         self.clock = pg.time.Clock()
         self.is_running = True
         self.title_screen = TitleScreen(self)
         self.mouse_pos = pg.mouse.get_pos()
+        self.mouse_x = self.mouse_pos[0]
+        self.mouse_y = self.mouse_pos[1]
 
     def run(self):
         while self.is_running:
             for event in pg.event.get():
-                if event.type == pg.QUIT:
+                if event.type == pg.QUIT: # Close window
                     self.is_running = False
-                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                if event.type == pg.MOUSEBUTTONDOWN and event.button == 1: # Mouse left-click
                     self.title_screen.play_button.clicked()
             
             self.mouse_pos = pg.mouse.get_pos()
+            self.mouse_x = self.mouse_pos[0]
+            self.mouse_y = self.mouse_pos[1]
 
             self.title_screen.draw()
 
@@ -65,14 +66,15 @@ class TitleScreen:
         self.play_button.draw()
 
     def play_button_clicked(self):
-        print("Clicked")
+        print("Play button clicked.")
 
 class Button:
     def __init__(self, game, x, y, image, action = None):
         self.game = game
         self.x = x
         self.y = y
-        self.image = pg.image.load(os.path.join(IMAGES_DIR, image))
+        self.image = pg.image.load(os.path.join(IMAGES_DIR, image)).convert_alpha()
+        self.mask = pg.mask.from_surface(self.image) # Creates mask of button
         self.rect = self.image.get_rect(center = (x, y))
         self.action = action
 
@@ -80,12 +82,11 @@ class Button:
         self.game.screen.blit(self.image, self.rect)
     
     def clicked(self):
-        if self.rect.collidepoint(new_game.mouse_pos):
-            self.action()
-
-
-    
-
+        if self.rect.collidepoint(self.game.mouse_pos): # First checks if button rectangle is clicked
+            pixel_x = self.game.mouse_x - self.rect.left # Relative x-coord of image pixel clicked
+            pixel_y = self.game.mouse_y - self.rect.top # Relative y-coord of image pixel clicked
+            if self.mask.get_at((pixel_x, pixel_y)): # Checks if pixel clicked is not transparent
+                self.action()
 
 # Code execution
 new_game = Game()
